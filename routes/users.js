4c1c01c8c0100e63d6d7a33ff4d0c8f908ba1321
brafdlog <brafdlog@gmail.com>
2014-10-24 18:45:16 +0300

    Add methods for getting specific user and adding user
    Former-commit-id: aca96c8fb8389c825a2b615903af10deab222dec

--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,48 @@ var express = require('express');
 var router = express.Router();
 
 // Get all users
-router.get('/', function(req, res) {
+router.get('/', function(req, resp) {
 	userDAO.getAllUsers(function(err, data) {
-		res.send(data);	
+		if (err) {
+			_handleError(resp, err);
+		} else {
+			resp.send(data);
+			resp.end();
+		}
 	});
 });
+
+// Get specific user
+router.get('/:username', function(req, resp) {
+	var username = req.params.username;
+	userDAO.getUser(username, function(err, data) {
+		if (err) {
+			_handleError(resp, err);
+		} else {
+			resp.send(data);
+			resp.end();
+		}
+	});
+});
+
+// Create user
+router.post('/', function(req, resp) {
+	console.log("Got request to create user");
+	var userToCreate = {username: req.body.username, password: req.body.password};
+	userDAO.createUser(userToCreate, function(err, createdUser) {
+		if (err) {
+			_handleError(resp, err);
+		} else {
+			console.log("Created user" + createdUser);
+			resp.send(createdUser);
+			resp.end();
+		}
+	});
+});
+
+function _handleError(resp, error) {
+	console.log("Error:" + error);
+	resp.status(500).end(error);
+}
 
 module.exports = router;
--- userDAO.js
+++ userDAO.js
@@ -1,14 +1,25 @@
-var mongo = require("mongodb");
 var monk = require("monk");
 
+var db = monk('localhost:27017/mydb');
+
 function getAllUsers(callback) {
-	var db = _getDBConnection();
 	var userCollection = db.get("users");
 	userCollection.find({}, callback);
 }
 
-function _getDBConnection() {
-	return monk('localhost:27017/mydb');
+function getUser(usrname, callback) {
+	var userCollection = db.get("users");
+	userCollection.find({username: usrname}, callback);
+}
+
+function createUser(userToCreate, callback) {
+	console.log("Creating user " + userToCreate);
+	var userCollection = db.get("users");
+	userCollection.insert(userToCreate, function (err, createdUser) {
+		callback(err, createdUser);
+	});
 }
 
 exports.getAllUsers = getAllUsers;
+exports.getUser = getUser;
+exports.createUser = createUser;
