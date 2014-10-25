e16736d21ebf82fb6ffe9e584a6dd41a07a74779
brafdlog <brafdlog@gmail.com>
2014-10-25 14:30:27 +0300

    UserDAO: 1. add logging for all methods. 2. add delete user method 3. add check that callback is defined before calling it Former-commit-id: bd2e6ab4d089615454ea2fd6154d7aaaca9df0af

--- userDAO.js
+++ userDAO.js
@@ -4,22 +4,61 @@ var db = monk('localhost:27017/mydb');
 
 function getAllUsers(callback) {
 	var userCollection = db.get("users");
-	userCollection.find({}, callback);
+	userCollection.find({}, function(err, allUsers) {
+		logMsg(err, "Loaded all users", "Failed loading all users. ");
+		if (_isFunction(callback)) {
+			callback(err, allUsers);
+		}
+	});
 }
 
 function getUser(usrname, callback) {
 	var userCollection = db.get("users");
-	userCollection.find({username: usrname}, callback);
+	userCollection.find({username: usrname}, function(err, foundUsersArray) {
+		logMsg(err, "Loaded user " + usrname, "Failed loading user " + usrname + ". Error ");
+	
+		if (_isFunction(callback)) {
+			callback(foundUsersArray);
+		}
+	});
 }
 
 function createUser(userToCreate, callback) {
-	console.log("Creating user " + userToCreate);
 	var userCollection = db.get("users");
 	userCollection.insert(userToCreate, function (err, createdUser) {
-		callback(err, createdUser);
+		logMsg(err, "Created user " + createdUser.username, "Failed creating user. Error ");
+	
+		if (_isFunction(callback)) {
+			callback(err, createdUser);
+		}
 	});
+}
+
+function deleteUser(usernameToDelete, callback) {
+	var userCollection = db.get("users");
+	userCollection.remove({username: usernameToDelete}, function (err) {
+		logMsg(err, "Deleted user " + usernameToDelete, "Failed deleteing user " + usernameToDelete + ". ");
+		
+		if(_isFunction(callback)) {
+			callback(err);
+		}
+		
+	});
+}
+
+function _isFunction(varToCheck) {
+	return (typeof(varToCheck) == "function");
+}
+
+function logMsg(err, successMsg, errorMsg) {
+	if (err) {
+		console.log(errorMsg + err);
+	} else {
+		console.log(successMsg);
+	}
 }
 
 exports.getAllUsers = getAllUsers;
 exports.getUser = getUser;
 exports.createUser = createUser;
+exports.deleteUser = deleteUser;
