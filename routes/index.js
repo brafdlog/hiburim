7f1309f39d5a18cc3194a72fdf06b9980a23e96d
brafdlog <brafdlog@gmail.com>
2014-10-26 21:01:31 +0200

    Make root redirect to users
    Former-commit-id: 015a4b7379e474c3937927ac68a2d5897b51f248

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { 'layout': 'generalLayout', 'title': 'moo' });
+ 	res.redirect("/users");
+ // res.render('index', { 'layout': 'generalLayout', 'title': 'moo' });
 });
 
 module.exports = router;
