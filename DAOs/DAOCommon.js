b6d3b6cac2f746a857009141160d72f2c1113e20
brafdlog <brafdlog@gmail.com>
2014-10-31 00:42:31 +0200

    Make DAOCommons a generic DAO and make carDAO use it
    Former-commit-id: 623c6bf63648fe0dc05fce91cdfe22acb96a3822

--- DAOs/DAOCommon.js
+++ DAOs/DAOCommon.js
@@ -1,9 +1,80 @@
+var monk = require("monk");
+var db = monk('localhost:27017/mydb');
 
-function isFunction(varToCheck) {
+function getAllElementsOfCollection(collectionName, callback) {
+	var collection = db.get(collectionName);
+	collection.find({}, function(err, allElementsOfCollection) {
+		_logMsg(err, "Loaded all " + collectionName, "Failed loading all " + collectionName +". ");
+		if (_isFunction(callback)) {
+			callback(err, allElementsOfCollection);
+		}
+	});
+}
+
+function getSingleElementOfCollection(collectionName, elementId, callback) {
+	var collection = db.get(collectionName);
+	collection.find({_id: elementId}, function(err, foundElementsArray) {
+		_logMsg(err, "Loaded element with id: " + elementId + " from collection" + collectionName, 
+			"Failed loading element with id: " + elementId + " from collection" + collectionName + ". Error ");
+
+		if (_isFunction(callback)) {
+			if (foundElementsArray && foundElementsArray.length === 1) {
+				callback(err, foundElementsArray[0]);	
+				return;
+			}
+			if (foundElementsArray.length > 1) {
+				err = "Found more than one element with id " + elementId + " from collection " + collectionName;
+				console.log(err);
+				callback(err);
+			} else { // if there is no element with the given id
+				callback(err);	
+			}
+		}
+	});
+}
+
+function createElement(collectionName, elementToCreate, callback) {
+	var collection = db.get(collectionName);
+	collection.insert(elementToCreate, function (err, createdElement) {
+		_logMsg(err, "Created element with id " + createdElement._id + " in collection " + collectionName, 
+			"Failed creating " + collectionName + ". Error ");
+
+		if (_isFunction(callback)) {
+			callback(err, createdElement);
+		}
+	});
+}
+
+function deleteElement(collectionName, elementIdToDelete, callback) {
+	var collection = db.get(collectionName);
+
+	getSingleElementOfCollection(collectionName, elementIdToDelete, function(err, elementToDelete) {
+		// Need to check this. If elementToDelete is undefined the delete call will delete the whole collection!
+		if (!elementToDelete) {
+			var errorString = "Failed deleting element with id " + elementIdToDelete + " from collection " + collectionName + 
+				". getElement on that id did not return an element";
+			console.log(errorString);
+			if (!err) {
+				err = errorString;
+			}
+			callback(err);
+			return;
+		}
+		collection.remove(elementToDelete, function (err) {
+			_logMsg(err, "Deleted element with id " + elementIdToDelete + " from collection " + collectionName, 
+				"Failed deleteing element with id " + elementIdToDelete + " from collection " + collectionName);
+			if(_isFunction(callback)) {
+				callback(err);
+			}				
+		});
+	});
+}
+	
+function _isFunction(varToCheck) {
 	return (typeof(varToCheck) == "function");
 }
 
-function logMsg(err, successMsg, errorMsg) {
+function _logMsg(err, successMsg, errorMsg) {
 	if (err) {
 		console.log(errorMsg + err);
 	} else {
@@ -11,5 +82,7 @@ function logMsg(err, successMsg, errorMsg) {
 	}
 }
 
-exports.isFunction = isFunction;
-exports.logMsg = logMsg;
+exports.getAllElementsOfCollection = getAllElementsOfCollection;
+exports.getSingleElementOfCollection = getSingleElementOfCollection;
+exports.createElement = createElement;
+exports.deleteElement = deleteElement;
--- DAOs/carDAO.js
+++ DAOs/carDAO.js
@@ -1,71 +1,19 @@
-var monk = require("monk");
 var DAOCommon = require("./DAOCommon");
 
-var db = monk('localhost:27017/mydb');
-
 function getAllCars(callback) {
-	var carCollection = db.get("cars");
-	carCollection.find({}, function(err, allCars) {
-		DAOCommon.logMsg(err, "Loaded all cars", "Failed loading all cars. ");
-		if (DAOCommon.isFunction(callback)) {
-			callback(err, allCars);
-		}
-	});
+	DAOCommon.getAllElementsOfCollection("cars", callback);
 }
 
 function getCar(carId, callback) {
-	var carCollection = db.get("cars");
-	carCollection.find({_id: carId}, function(err, foundCarsArray) {
-		DAOCommon.logMsg(err, "Loaded car " + carId, "Failed loading car " + carId + ". Error ");
-
-		if (DAOCommon.isFunction(callback)) {
-			if (foundCarsArray && foundCarsArray.length === 1) {
-				callback(err, foundCarsArray[0]);	
-				return;
-			}
-			if (foundCarsArray.length > 1) {
-				err = "Found more than one car for id " + carId;
-				console.log(err);
-				callback(err);
-			} else { // if there is no car with the given id
-				callback(err);	
-			}
-		}
-	});
+	DAOCommon.getSingleElementOfCollection("cars", carId, callback);
 }
 
 function createCar(carToCreate, callback) {
-	var carCollection = db.get("cars");
-	carCollection.insert(carToCreate, function (err, createdCar) {
-		DAOCommon.logMsg(err, "Created car " + createdCar._id, "Failed creating car. Error ");
-
-		if (DAOCommon.isFunction(callback)) {
-			callback(err, createdCar);
-		}
-	});
+	DAOCommon.createElement("cars", carToCreate, callback);
 }
 
 function deleteCar(carIdToDelete, callback) {
-	var carCollection = db.get("cars");
-
-	getCar(carIdToDelete, function(err, carToDelete) {
-		// Need to check this. If carToDelete is undefined the delete call will delete the whole collection!
-		if (!carToDelete) {
-			var errorString = "Failed deleting car with id " + carIdToDelete + ". Get car on that id did not return a car";
-			console.log(errorString);
-			if (!err) {
-				err = "Failed deleting car with id " + carIdToDelete + ". Get car on that id did not return a car";
-			}
-			callback(err);
-			return;
-		}
-		carCollection.remove(carToDelete, function (err) {
-			DAOCommon.logMsg(err, "Deleted car with id " + carIdToDelete, "Failed deleteing car with id " + carIdToDelete + ". ");
-			if(DAOCommon.isFunction(callback)) {
-				callback(err);
-			}				
-		});
-	});
+	DAOCommon.deleteElement("cars", carIdToDelete, callback);
 }
 
 exports.getAllCars = getAllCars;
