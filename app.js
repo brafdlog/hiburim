6b57de7b188b4fbccf795f17c514ec08459cea12
brafdlog <brafdlog@gmail.com>
2014-10-31 12:32:40 +0200

    Add consumer route and js
    Former-commit-id: a9d267d9ae4fbcc01531dddd9b631639a9f78249

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var cars = require('./routes/cars');
 var donors = require('./routes/donors');
+var consumers = require('./routes/consumers');
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/cars', cars);
 app.use('/donors', donors);
+app.use('/consumers', consumers);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/consumers.js
@@ -0,0 +1,94 @@
+var routesCommon = require("./routesCommon");
+var consumerDAO = require("../DAOs/consumerDAO");
+var express = require('express');
+var _ = require("underscore");
+var router = express.Router();
+
+// Get all consumers
+router.get('/', function(req, resp) {	
+	consumerDAO.getAllConsumers(function(err, allConsumersArray) {
+		if (err) {
+			routesCommon.handleServerError(resp, err);
+		} else {
+			resp.send(allConsumersArray);
+			resp.end();
+			//resp.render('consumers', {'layout': 'generalLayout', consumers: allConsumersArray});
+		}
+	});
+});
+
+// Get specific consumer
+router.get('/:consumerId', function(req, resp) {
+	var consumerId = req.params.consumerId;
+	consumerDAO.getConsumer(consumerId, function(err, consumerFromDB) {
+		if (err) {
+			routesCommon.handleServerError(resp, err);
+		} else {
+			if (consumerFromDB) {
+				resp.send(consumerFromDB);
+			} else {
+				resp.send("Consumer with id " + consumerId + " was not found");
+			}
+			resp.end();
+		}
+	});
+});
+
+// Create consumer
+router.post('/', function(req, resp) {
+	console.log("Got request to create a consumer");
+	var consumerToCreate = _createConsumerObject(req.body.name, req.body.phoneNumber, req.body.convenientDates, req.body.itemDetails, req.body.addressDetails);
+	consumerDAO.createConsumer(consumerToCreate, function(err, createdConsumer) {
+		if (err) {
+			routesCommon.handleServerError(resp, err);
+		} else {
+			resp.send(createdConsumer);
+			resp.end();
+		}
+	});
+});
+
+// Delete specific consumer
+router.delete('/:consumerId', function(req, resp) {
+	var consumerId = req.params.consumerId;
+	consumerDAO.deleteConsumer(consumerId, function(err) {
+		if (err) {
+			routesCommon.handleServerError(resp, err);
+		} else {
+			resp.status(200).end();
+		}
+	});
+});
+
+
+function _createConsumerObject(name, phoneNumber, convenientDates, itemDetails, addressDetails) {
+	return {name: name, phoneNumber: phoneNumber, convenientDates: convenientDates, item: itemDetails, address: addressDetails};
+}
+
+module.exports = router;
+
+/*
+	Consumer object:
+	 {
+		name: "Moshe ", 
+		phoneNumber: "057-456732",
+		convenientDates: [456345634564],
+		item: {
+			category: "ארון",
+			requiresDismanteling: true,
+			description: "זה ארון מאוד יפה"
+		},
+		address: {
+			geoQueryString: "Even Shmuel 33, Jerusalem, Israel",
+			geoDisplayString: "אבן שמואל 33, ירושלים",
+			latitude: "4536364.534",
+			longitude: "645645645.3423",
+			floor: "3",
+			flatNumber: "7",
+			hasElavator: false,
+			hasParking: true,
+			description: "מקום יפה"
+		}
+	};
+
+ */
