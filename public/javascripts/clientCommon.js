432bb2e278a6e92a4f7cd9f20d7f9996edd6bc62
brafdlog <brafdlog@gmail.com>
2014-11-04 22:37:36 +0200

    Create a clientCommons js file and implmement my own post function
    Former-commit-id: 6480e8048b2b0f410506acff712a5e0e13a1a428

---
+++ public/javascripts/clientCommon.js
@@ -0,0 +1,12 @@
+
+$.myPost = function(url, data, success, error) {
+	$.ajax({
+		type: "POST",
+		url: url,
+		data: JSON.stringify(data),
+		success: success,
+		error: error,
+		contentType: 'application/json;charset=UTF-8',
+		dataType: 'json'
+	});
+}
