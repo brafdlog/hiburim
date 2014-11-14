562d0e4e35bf85d48853e7ac72f7f58b49b7bd7f
brafdlog <brafdlog@gmail.com>
2014-11-15 01:22:41 +0200

    Add sorting and filtering to cars table
    Former-commit-id: a4ab98b4e14f3fd162e2aac9afac9dfcf467eb13

--- public/javascripts/ember/controllers.js
+++ public/javascripts/ember/controllers.js
@@ -1,4 +1,14 @@
-App.CarsController = Ember.ObjectController.extend({
+App.CarsController = Ember.ArrayController.extend({
+	filter: '',
+
+	filteredCars: function() {
+		var filter = this.get('filter');
+		var regEx = new RegExp(filter, 'gi');
+		var cars = this.get('arrangedContent');
+		return cars.filter(function(car) {
+			return car.get('carType').match(regEx) || car.get('driverName').match(regEx);
+		});
+	}.property('filter', 'arrangedContent', 'arrangedContent.@each.isDeleted'),
 	actions: {
 		delete: function(carId) {
 			var that = this;
@@ -6,10 +16,14 @@ App.CarsController = Ember.ObjectController.extend({
 				if (userWantsToDelete) {
 					that.store.find('car', carId).then(function(car) {
 						car.deleteRecord();
-						car.save();	
+						car.save();
 					});
 				}
 			});
+		},
+		sortBy: function(property) {
+			this.set('sortProperties', [property]);
+			this.set('sortAscending', !this.get('sortAscending'));
 		}
 	}
 });
