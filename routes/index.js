d161d41ae76f0be08bada0fbf84fd6cf8855f002
brafdlog <brafdlog@gmail.com>
2014-11-18 23:39:20 +0200

    Make the ember.html the default
    Former-commit-id: fc820b0b3585bb351aec10fdaf3acdcd61bc229e

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
- 	res.redirect("/cars");
+ 	res.redirect("/ember.html#/cars");
  // res.render('index', { 'layout': 'generalLayout', 'title': 'moo' });
 });
 
