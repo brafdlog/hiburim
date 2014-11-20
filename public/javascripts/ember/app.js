16cbf507639e4a2135daf536d330294e47df207e
brafdlog <brafdlog@gmail.com>
2014-11-21 00:43:29 +0200

    Work with fixture
    Former-commit-id: 8dbfc1aa3e2e22b12ffe34289cc54abbdbf0386e

--- public/javascripts/ember/app.js
+++ public/javascripts/ember/app.js
@@ -1,7 +1,7 @@
 App = Ember.Application.create();
 
 // This is for using fictures
-// App.ApplicationAdapter = DS.FixtureAdapter;
+App.ApplicationAdapter = DS.FixtureAdapter;
 
 // This should allow using _id instead of id in models but doesn't work
 // App.ApplicationSerializer = DS.RESTSerializer.extend({
