08c4fd5b26669953dd8df8b3dc747c8bb41aa5ba
brafdlog <brafdlog@gmail.com>
2014-11-21 01:02:16 +0200

    Add consumer model and fixture
    Former-commit-id: 3d33f95e3e706af94b6a0226c806dfe81e614749

--- public/javascripts/ember/models.js
+++ public/javascripts/ember/models.js
@@ -15,3 +15,75 @@ App.Car.reopenClass({
 		{"_id":"546d1af50f58e515436c9391","carType":"רכב פרטי","driverName":"חיים","driverNumber":"07-893245","availableFromDateTime":"2014-11-11T04:30:00.000Z","availableDurationInHours":6,"availableUntilDateTime":"2014-11-11T10:30:00.000Z","id":"546d1af50f58e515436c9391"},{"_id":"546d1ba47b9e2db243e5836f","carType":"רכב מסחרי","driverName":"מיכל","driverNumber":"052-556234","availableFromDateTime":"2014-11-04T05:00:00.000Z","availableDurationInHours":8,"availableUntilDateTime":"2014-11-04T13:00:00.000Z","id":"546d1ba47b9e2db243e5836f"}
 	]
 });
+
+App.Consumer = DS.Model.extend({
+	_id: DS.attr(),
+	name: DS.attr(), 
+	phoneNumber: DS.attr(),
+	convenientDates: DS.attr(),
+	item: {
+		category: DS.attr(),
+		requiresDismanteling: DS.attr('boolean'),
+		description: DS.attr()
+	},
+	address: {
+		geoQueryString: DS.attr(),
+		geoDisplayString: DS.attr(),
+		latitude: DS.attr(),
+		longitude: DS.attr(),
+		floor: DS.attr('number'),
+		flatNumber: DS.attr('number'),
+		hasElavator: DS.attr('boolean'),
+		hasParking: DS.attr('boolean'),
+		description: DS.attr()
+	}
+});
+
+App.Consumer.reopenClass({
+	FIXTURES: [
+	{
+		_id: 1,
+		name: 'משה', 
+		phoneNumber: '02-56835213',
+		convenientDates: '2/4/14, 5/6/14',
+		item: {
+			category: 'ארון',
+			requiresDismanteling: true,
+			description: 'זה ארון מאוד יפה'
+		},
+		address: {
+			geoQueryString: "Dubnov 10, Tel Aviv",
+			geoDisplayString: 'דובנוב 10 תל אביב',
+			latitude: "4536364.534",
+			longitude: "645645645.3423",
+			floor: 2,
+			flatNumber: 1,
+			hasElavator: true,
+			hasParking: false,
+			description: 'כתובת מאוד מעניינת'
+		}
+	},
+	{
+		_id: 2,
+		name: 'חיים', 
+		phoneNumber: "057-456732",
+		convenientDates: '6/3/14, 9/5/14',
+		item: {
+			category: "שידה",
+			requiresDismanteling: false,
+			description: "וואי וואי איזה שידה"
+		},
+		address: {
+			geoQueryString: "Even Shmuel 33, Jerusalem, Israel",
+			geoDisplayString: "אבן שמואל 33, ירושלים",
+			latitude: "4536364.534",
+			longitude: "645645645.3423",
+			floor: "3",
+			flatNumber: "7",
+			hasElavator: false,
+			hasParking: true,
+			description: "מקום יפה"
+		}
+	}
+	]
+});
