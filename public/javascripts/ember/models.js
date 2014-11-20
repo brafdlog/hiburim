dcef9e76f94e7f9bb6bea000f94a96a1378f1103
brafdlog <brafdlog@gmail.com>
2014-11-21 01:00:15 +0200

    Update cars fixture
    Former-commit-id: 5b15442144639bc7b0c9a6e57b917f4a3f765fc9

--- public/javascripts/ember/models.js
+++ public/javascripts/ember/models.js
@@ -10,8 +10,8 @@ App.Car = DS.Model.extend({
 
 App.Car.reopenClass({
 	FIXTURES: [
-		{"id":"1", "_id":"1","carType":"van","driverName":"שלומית","driverNumber":"02-5867324","availableFromDate":"04-11-2014","availableFromTime":"7:30","availableDurationInHours":"6","van":true},
-		{"id":"2", "_id":"2","carType":"car","driverName":"דוד","driverNumber":"054-234145","availableFromDate":"02-11-2014","availableFromTime":"9:15","availableDurationInHours":"2","van":true},
-		{"id":"3", "_id":"3","carType":"van","driverName":"חיים","driverNumber":"07-678234","availableFromDate":"23-11-2014","availableFromTime":"17:25","availableDurationInHours":"5","van":true}
+		{"_id":"546bad8e505448f20cb353d1","carType":"לא זמין","driverName":"דוד הגשש","driverNumber":"054-8729124","availableFromDateTime":"2014-11-02T07:00:00.000Z","availableDurationInHours":2,"availableUntilDateTime":"2014-11-02T09:00:00.000Z","id":"546bad8e505448f20cb353d1"},
+		{"_id":"546c4988beb4c8487856ff27","carType":"רכב מסחרי","driverName":"אל","driverNumber":"04-8963256","availableFromDateTime":"2014-11-03T09:30:00.000Z","availableDurationInHours":9,"availableUntilDateTime":"2014-11-03T18:30:00.000Z","id":"546c4988beb4c8487856ff27"},
+		{"_id":"546d1af50f58e515436c9391","carType":"רכב פרטי","driverName":"חיים","driverNumber":"07-893245","availableFromDateTime":"2014-11-11T04:30:00.000Z","availableDurationInHours":6,"availableUntilDateTime":"2014-11-11T10:30:00.000Z","id":"546d1af50f58e515436c9391"},{"_id":"546d1ba47b9e2db243e5836f","carType":"רכב מסחרי","driverName":"מיכל","driverNumber":"052-556234","availableFromDateTime":"2014-11-04T05:00:00.000Z","availableDurationInHours":8,"availableUntilDateTime":"2014-11-04T13:00:00.000Z","id":"546d1ba47b9e2db243e5836f"}
 	]
 });
