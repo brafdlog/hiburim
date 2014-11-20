cf7b2fdd5b9aaf38402304dc5c322ec119c2ad36
brafdlog <brafdlog@gmail.com>
2014-11-21 01:02:31 +0200

    Add consumer route
    Former-commit-id: 86fe214ac1196b41302da386b335f7f1003dd7de

--- public/javascripts/ember/router.js
+++ public/javascripts/ember/router.js
@@ -2,5 +2,7 @@ App.Router.map(function() {
 	this.resource('cars', function() {
 		this.resource('car', {path: ':car_id'});
 	});
-	this.resource('consumers');
+	this.resource('consumers', function() {
+		this.resource('consumer', {path: ':consumer_id'});
+	});
 });
