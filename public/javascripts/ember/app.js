f733a16dd9b2e2f8420d6083c9b5409607e7224f
brafdlog <brafdlog@gmail.com>
2014-11-22 12:52:54 +0200

    Handle fixture/real data configuration in a boolean var instead of commenting out
    Former-commit-id: eeb32e1281439bf8d95dd9a8d1ebeb3d5309ebd2

--- public/javascripts/ember/app.js
+++ public/javascripts/ember/app.js
@@ -1,16 +1,22 @@
 App = Ember.Application.create();
 
-// This is for using fixtures
-App.ApplicationAdapter = DS.FixtureAdapter;
+var useRealData = true;
 
-// This should allow using _id instead of id in models
-// App.ApplicationSerializer = DS.RESTSerializer.extend({
-//   primaryKey: '_id',
-//   // Default serialization of save doesn't pass in the id in the post body 
-//   // so we add it here manually
-//   serialize: function(record, options) {
-//     var json = this._super.apply(this, arguments); // Get default serialization
-//     json._id = record.id;  // tack on the id
-//     return json;
-//   }
-// });
+if (useRealData) {
+	// This should allow using _id instead of id in models
+	App.ApplicationSerializer = DS.RESTSerializer.extend({
+		primaryKey: '_id',
+  		// Default serialization of save doesn't pass in the id in the post body 
+  		// so we add it here manually
+  		serialize: function(record, options) {
+    		var json = this._super.apply(this, arguments); // Get default serialization
+    		json._id = record.id;  // tack on the id
+    		return json;
+    	}
+    });
+} else {
+	// This is for using fixtures
+	App.ApplicationAdapter = DS.FixtureAdapter;	
+}
+
+
