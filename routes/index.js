731e69e471094d32a270d75076d36418129b3818
brafdlog <brafdlog@gmail.com>
2015-02-20 15:51:08 +0200

    redirect to donors screen and not to cars
    Former-commit-id: bc0cc1707360a1eb3e87ca797718c010e95416cc

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
- 	res.redirect("/ember.html#/cars");
+ 	res.redirect("/ember.html#/donors");
 });
 
 router.post('/login',
